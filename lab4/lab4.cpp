d4a424192d997c7483b749396cfcf7864e2dfc69
KRIDSAYA NATEEMONTON <63010026@KMITL.AC.TH>
2020-09-10 17:40:34 +0700

    update

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -12,20 +12,29 @@ void gotoxy(int x, int y)
 
 void draw_ship(int x, int y)
 {
+	gotoxy(x, y);
 	printf("<-0->");
+}
+
+void erase_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf("     ");
 }
 
 int main()
 {
 	char ch = ' ';
 	int x = 38, y = 20;
-	draw_ship(38, 20);
+	draw_ship(x, y);
 	do {
 		if (_kbhit())
 		{
 			ch = _getch();
-			if (ch == 'a') { draw_ship(--x, y); }
-			if (ch == 'd') { draw_ship(++x, y); }
+			if (ch == 'a' && x > 0) { erase_ship(x, y); draw_ship(--x, y); }
+			if (ch == 'd' && x < 75) { erase_ship(x, y); draw_ship(++x, y); }
+			if (ch == 's' && y < 22) { erase_ship(x, y); draw_ship(x, ++y); }
+			if (ch == 'w' && y > 0) { erase_ship(x, y); draw_ship(x, --y); }
 			fflush(stdin);
 		}
 		Sleep(100);
